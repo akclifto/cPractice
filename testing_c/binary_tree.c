f89c1b89155b3a98bbd769ffd2cbbcf0b8871632
Adam ASU <56737464+akclifto@users.noreply.github.com>
2020-02-13 10:37:45 -0700

    Binary Tree DFS, pre-order exercise

---
+++ testing_c/binary_tree.c
@@ -0,0 +1,85 @@
+/*Learn-c.org Binary Tree Exercise
+    Below is an implementation of a binary tree that has insertion and printing capabilities.
+    This tree is ordered but not balanced. 
+    This example maintains its ordering at insertion time.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+  int val;
+  struct node * left;
+  struct node * right;
+} node_t;
+
+void insert(node_t * tree,int val);
+void print_tree(node_t * current);
+void printDFS(node_t * current);
+
+int main()
+{
+  node_t * test_list = (node_t *) malloc(sizeof(node_t));
+  /* set values explicitly, alternative would be calloc() */
+  test_list->val = 0;
+  test_list->left = NULL;
+  test_list->right = NULL;
+
+  /*insert test values in tree*/
+  insert(test_list,5);
+  insert(test_list,8);
+  insert(test_list,4);
+  insert(test_list,3);
+
+  printDFS(test_list);
+  printf("\n");
+}
+
+void insert(node_t * tree, int val)
+{
+  if (tree->val == 0) {
+    /* insert on current (empty) position */
+    tree->val = val;
+  } else {
+
+    if (val < tree->val) {
+      /* insert left */
+      if (tree->left != NULL) {
+        insert(tree->left, val);
+      } else {
+
+        tree->left = (node_t *) malloc(sizeof(node_t));
+        /* set values explicitly, alternative would be calloc() */
+        tree->left->val = val;
+        tree->left->left = NULL;
+        tree->left->right = NULL;
+      }
+    } else {
+        
+      if (val >= tree->val) {
+        /* insert right */
+        if (tree->right != NULL) {
+          insert(tree->right,val);
+        } else {
+          tree->right = (node_t *) malloc(sizeof(node_t));
+          /* set values explicitly, alternative would be calloc() */
+          tree->right->val = val;
+          tree->right->left = NULL;
+          tree->right->right = NULL;
+        }
+      }
+    }
+  }
+}
+
+/* depth-first search, pre-order */
+void printDFS(node_t * current)
+{
+  /* change the code here. CHANGED */
+  if (current == NULL)         return;   /* security measure */
+  if (current != NULL)         printf("%d ", current->val);  // read the current node in the tree.
+  if (current->left != NULL)   printDFS(current->left);      // check left, then recursive call
+  if (current->right != NULL)  printDFS(current->right);     // check right, then recursive call
+
+}
